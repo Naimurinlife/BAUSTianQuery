9fdd1899ccbd61abfd67ad5baf83b1c8e593b8dd
Naimur <naimurshuvo.bd.16@gmail.com>
2020-10-02 19:05:31 +0600

    First Version

---
+++ threadlist.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <title>BAUSTian Discussion</title>
+</head>
+
+<body>
+    <?php include 'partials/_header.php';  ?>
+    <?php include 'partials/_dbconnect.php';  ?>
+
+    <div class="container my-4">
+
+        <div class="jumbotron">
+            <h1 class="display-4">Welcome to CSE Forum</h1>
+            <p class="lead">Welcome to the FORUM of Computer Science and Engineering (CSE)</p>
+            <hr class="my-4">
+            <p>This is a Forum Which can be used to share knowledge about Recent of Tech related discussion.</p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </div>
+
+
+    </div>
+    <div class="container">
+        <h1 class="py-3">Browse Questions</h1>
+
+        <div class="media my-3">
+            <img src="img/default_user.png" width="54px" class="mr-3" alt="...">
+            <div class="media-body">
+                <h5 class="mt-0">Unable to install Android Studio SDK</h5>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+        </div>
+        <div class="media my-3">
+            <img src="img/default_user.png" width="54px" class="mr-3" alt="...">
+            <div class="media-body">
+                <h5 class="mt-0">Unable to install Android Studio SDK</h5>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+        </div>
+        <div class="media my-3">
+            <img src="img/default_user.png" width="54px" class="mr-3" alt="...">
+            <div class="media-body">
+                <h5 class="mt-0">Unable to install Android Studio SDK</h5>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+        </div>
+        <div class="media my-3">
+            <img src="img/default_user.png" width="54px" class="mr-3" alt="...">
+            <div class="media-body">
+                <h5 class="mt-0">Unable to install Android Studio SDK</h5>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+        </div>
+        <div class="media my-3">
+            <img src="img/default_user.png" width="54px" class="mr-3" alt="...">
+            <div class="media-body">
+                <h5 class="mt-0">Unable to install Android Studio SDK</h5>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+        </div>
+        <div class="media my-3">
+            <img src="img/default_user.png" width="54px" class="mr-3" alt="...">
+            <div class="media-body">
+                <h5 class="mt-0">Unable to install Android Studio SDK</h5>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+        </div>
+        <div class="media my-3">
+            <img src="img/default_user.png" width="54px" class="mr-3" alt="...">
+            <div class="media-body">
+                <h5 class="mt-0">Unable to install Android Studio SDK</h5>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+        </div>
+    </div>
+
+
+    <?php include 'partials/_footer.php';  ?>
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+
+</html>
